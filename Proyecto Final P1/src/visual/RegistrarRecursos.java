7f67e1d15e375a704b942cf37a9c34b52cbd63d3
LATITUDE <LATITUDE@DESKTOP-25N0HVF>
2019-11-14 16:21:48 -0400

    Registrar Recursos

---
+++ Proyecto Final P1/src/visual/RegistrarRecursos.java
@@ -0,0 +1,66 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
+
+public class RegistrarRecursos extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			RegistrarRecursos dialog = new RegistrarRecursos();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public RegistrarRecursos() {
+		setTitle("Registrar Recursos");
+		setBounds(100, 100, 254, 153);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Tipo:");
+		lblNewLabel.setBounds(29, 22, 46, 14);
+		contentPanel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setText("");
+		textField.setBounds(67, 19, 86, 20);
+		contentPanel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblCantidad = new JLabel("Cantidad:");
+		lblCantidad.setBounds(10, 47, 77, 14);
+		contentPanel.add(lblCantidad);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(67, 47, 86, 20);
+		contentPanel.add(spinner);
+		
+		JButton btnNewButton = new JButton("Registrar");
+		btnNewButton.setBounds(137, 78, 89, 23);
+		contentPanel.add(btnNewButton);
+	}
+
+}
