44299d9d7572475f3ba046cd65d1751bd7946ecc
Gabriel <20170388@ce.pucmm.edu.do>
2019-11-24 22:36:34 -0400

    Arreglando inconsistencia en el tipo de letra

--- Proyecto Final P1/src/visual/ModificarPersona.java
+++ Proyecto Final P1/src/visual/ModificarPersona.java
@@ -36,8 +36,11 @@ public class ModificarPersona extends JDialog {
 	private JTextField txtDireccion;
 	private JTextField txtTelefono;
 	private JRadioButton rdbtnMasculin;
+	private JRadioButton rdbtnMasculin_1;
 	private JRadioButton rdbtnFemenino;
+	private JRadioButton rdbtnFemenino_1;
 	private JComboBox comboBox;
+	private JComboBox comboBox_2;
 	private JPanel panel;
 	private JPanel panelJuez;
 	
@@ -93,95 +96,97 @@ public class ModificarPersona extends JDialog {
 			contentPanel.add(panel, "name_174943819359200");
 			{
 				JLabel label = new JLabel("C\u00E9dula:");
-				label.setFont(new Font("Tahoma", Font.PLAIN, 13));
-				label.setBounds(17, 41, 73, 23);
+				label.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+				label.setBounds(17, 39, 73, 23);
 				panel.add(label);
 			}
 			{
 				JLabel label = new JLabel("Nombre:");
-				label.setFont(new Font("Tahoma", Font.PLAIN, 13));
-				label.setBounds(17, 105, 88, 23);
+				label.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+				label.setBounds(17, 101, 88, 23);
 				panel.add(label);
 			}
 			{
 				JLabel label = new JLabel("Direcci\u00F3n:");
-				label.setFont(new Font("Tahoma", Font.PLAIN, 13));
-				label.setBounds(17, 169, 88, 23);
+				label.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+				label.setBounds(17, 163, 88, 23);
 				panel.add(label);
 			}
 			{
 				JLabel label = new JLabel("Tel\u00E9fono:");
-				label.setFont(new Font("Tahoma", Font.PLAIN, 13));
-				label.setBounds(17, 233, 88, 23);
+				label.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+				label.setBounds(17, 225, 88, 23);
 				panel.add(label);
 			}
 			{
 				txtCedula = new JTextField();
-				txtCedula.setFont(new Font("Tahoma", Font.PLAIN, 13));
+				txtCedula.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 				txtCedula.setColumns(10);
-				txtCedula.setBounds(107, 38, 244, 29);
+				txtCedula.setBounds(196, 34, 293, 29);
 				txtCedula.setText(miPersona.getCedula());
 				panel.add(txtCedula);
 			}
 			{
 				txtNombre = new JTextField();
-				txtNombre.setFont(new Font("Tahoma", Font.PLAIN, 13));
+				txtNombre.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 				txtNombre.setColumns(10);
-				txtNombre.setBounds(107, 102, 244, 29);
+				txtNombre.setBounds(196, 98, 293, 29);
 				txtNombre.setText(miPersona.getNombre());
 				panel.add(txtNombre);
 			}
 			{
 				txtDireccion = new JTextField();
-				txtDireccion.setFont(new Font("Tahoma", Font.PLAIN, 13));
+				txtDireccion.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 				txtDireccion.setColumns(10);
-				txtDireccion.setBounds(107, 166, 244, 29);
+				txtDireccion.setBounds(196, 160, 293, 29);
 				txtDireccion.setText(miPersona.getDireccion());
 				panel.add(txtDireccion);
 			}
 			{
 				txtTelefono = new JTextField();
-				txtTelefono.setFont(new Font("Tahoma", Font.PLAIN, 13));
+				txtTelefono.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 				txtTelefono.setColumns(10);
-				txtTelefono.setBounds(107, 230, 244, 29);
+				txtTelefono.setBounds(196, 222, 293, 29);
 				txtTelefono.setText(miPersona.getTelefono());
 				panel.add(txtTelefono);
 			}
 			
 			JLabel label = new JLabel("Sexo:");
-			label.setFont(new Font("Tahoma", Font.PLAIN, 13));
-			label.setBounds(17, 297, 72, 16);
+			label.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+			label.setBounds(17, 287, 72, 16);
 			panel.add(label);
 			
 			rdbtnMasculin = new JRadioButton("Masculino");
+			rdbtnMasculin.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 			rdbtnMasculin.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					rdbtnMasculin.setSelected(true);
 					rdbtnFemenino.setSelected(false);
 				}
 			});
-			rdbtnMasculin.setBounds(107, 293, 100, 25);
+			rdbtnMasculin.setBounds(202, 283, 134, 25);
 			panel.add(rdbtnMasculin);
 			
 			rdbtnFemenino = new JRadioButton("Femenino");
+			rdbtnFemenino.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 			rdbtnFemenino.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					rdbtnMasculin.setSelected(false);
 					rdbtnFemenino.setSelected(true);
 				}
 			});
-			rdbtnFemenino.setBounds(230, 293, 112, 25);
+			rdbtnFemenino.setBounds(345, 283, 124, 25);
 			panel.add(rdbtnFemenino);
 			
 			JLabel label_1 = new JLabel("Grado Acad\u00E9mico:");
-			label_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-			label_1.setBounds(10, 354, 112, 16);
+			label_1.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+			label_1.setBounds(17, 342, 142, 16);
 			panel.add(label_1);
 			
 			comboBox = new JComboBox();
-			comboBox.setFont(new Font("Tahoma", Font.PLAIN, 13));
+			comboBox.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 			comboBox.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "Bachiller", "Licenciado", "Mag\u00EDster", "Profesional", "Doctorado"}));
-			comboBox.setBounds(132, 351, 219, 22);
+			comboBox.setBounds(196, 339, 293, 22);
 			panel.add(comboBox);
 		}
 		{
@@ -191,25 +196,25 @@ public class ModificarPersona extends JDialog {
 			contentPanel.add(panelJuez, "name_174943835546300");
 			{
 				JLabel label = new JLabel("Tel\u00E9fono:");
-				label.setBounds(17, 200, 88, 23);
+				label.setBounds(17, 197, 88, 23);
 				panelJuez.add(label);
-				label.setFont(new Font("Tahoma", Font.PLAIN, 11));
+				label.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 			}
 			{
 				JLabel label = new JLabel("C\u00E9dula:");
-				label.setFont(new Font("Tahoma", Font.PLAIN, 11));
+				label.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 				label.setBounds(17, 32, 73, 23);
 				panelJuez.add(label);
 			}
 			{
 				JLabel label = new JLabel("Nombre:");
-				label.setFont(new Font("Tahoma", Font.PLAIN, 11));
+				label.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 				label.setBounds(17, 87, 88, 23);
 				panelJuez.add(label);
 			}
 			{
 				JLabel label = new JLabel("Direcci\u00F3n:");
-				label.setFont(new Font("Tahoma", Font.PLAIN, 11));
+				label.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 				label.setBounds(17, 142, 88, 23);
 				panelJuez.add(label);
 			}
@@ -217,7 +222,7 @@ public class ModificarPersona extends JDialog {
 				txtCedula = new JTextField();
 				txtCedula.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 				txtCedula.setColumns(10);
-				txtCedula.setBounds(107, 29, 244, 29);
+				txtCedula.setBounds(192, 29, 297, 29);
 				txtCedula.setText(miPersona.getCedula());
 				panelJuez.add(txtCedula);
 			}
@@ -225,7 +230,7 @@ public class ModificarPersona extends JDialog {
 				txtNombre = new JTextField();
 				txtNombre.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 				txtNombre.setColumns(10);
-				txtNombre.setBounds(107, 84, 244, 29);
+				txtNombre.setBounds(192, 84, 297, 29);
 				txtNombre.setText(miPersona.getNombre());
 				panelJuez.add(txtNombre);
 			}
@@ -233,7 +238,7 @@ public class ModificarPersona extends JDialog {
 				txtDireccion = new JTextField();
 				txtDireccion.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 				txtDireccion.setColumns(10);
-				txtDireccion.setBounds(107, 139, 244, 29);
+				txtDireccion.setBounds(192, 139, 297, 29);
 				txtDireccion.setText(miPersona.getDireccion());
 				panelJuez.add(txtDireccion);
 			}
@@ -241,52 +246,59 @@ public class ModificarPersona extends JDialog {
 				txtTelefono = new JTextField();
 				txtTelefono.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 				txtTelefono.setColumns(10);
-				txtTelefono.setBounds(107, 194, 244, 29);
+				txtTelefono.setBounds(192, 194, 297, 29);
 				txtTelefono.setText(miPersona.getTelefono());
 				panelJuez.add(txtTelefono);
 			}
 			
 			JLabel label = new JLabel("Sexo:");
-			label.setBounds(17, 253, 72, 16);
+			label.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+			label.setBounds(17, 252, 72, 16);
 			panelJuez.add(label);
 			
-			rdbtnMasculin = new JRadioButton("Masculino");
-			rdbtnMasculin.addActionListener(new ActionListener() {
+			rdbtnMasculin_1 = new JRadioButton("Masculino");
+			rdbtnMasculin_1.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+			rdbtnMasculin_1.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					rdbtnMasculin.setSelected(true);
 					rdbtnFemenino.setSelected(false);
 				}
 			});
-			rdbtnMasculin.setBounds(107, 253, 100, 25);
-			panelJuez.add(rdbtnMasculin);
+			rdbtnMasculin_1.setBounds(192, 248, 124, 25);
+			panelJuez.add(rdbtnMasculin_1);
 			
-			rdbtnFemenino = new JRadioButton("Femenino");
-			rdbtnFemenino.addActionListener(new ActionListener() {
+			rdbtnFemenino_1 = new JRadioButton("Femenino");
+			rdbtnFemenino_1.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+			rdbtnFemenino_1.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					rdbtnMasculin.setSelected(false);
 					rdbtnFemenino.setSelected(true);
 				}
 			});
-			rdbtnFemenino.setBounds(230, 253, 112, 25);
-			panelJuez.add(rdbtnFemenino);
+			rdbtnFemenino_1.setBounds(347, 248, 142, 25);
+			panelJuez.add(rdbtnFemenino_1);
 			
 			JLabel label_1 = new JLabel("Grado Acad\u00E9mico:");
-			label_1.setBounds(10, 298, 112, 16);
+			label_1.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+			label_1.setBounds(17, 300, 165, 16);
 			panelJuez.add(label_1);
 			
-			comboBox = new JComboBox();
-			comboBox.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "Bachiller", "Licenciado", "Mag\u00EDster", "Profesional", "Doctorado"}));
-			comboBox.setBounds(132, 295, 238, 22);
-			panelJuez.add(comboBox);
+			comboBox_2 = new JComboBox();
+			comboBox_2.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+			comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "Bachiller", "Licenciado", "Mag\u00EDster", "Profesional", "Doctorado"}));
+			comboBox_2.setBounds(192, 297, 297, 22);
+			panelJuez.add(comboBox_2);
 			{
 				JLabel label_2 = new JLabel("\u00C1rea de estudio:");
-				label_2.setBounds(10, 348, 112, 16);
+				label_2.setFont(new Font("Times New Roman", Font.PLAIN, 19));
+				label_2.setBounds(17, 348, 165, 16);
 				panelJuez.add(label_2);
 			}
 			{
 				JComboBox comboBox_1 = new JComboBox();
+				comboBox_1.setFont(new Font("Times New Roman", Font.PLAIN, 19));
 				comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "Matem\u00E1ticas", "Qu\u00EDmica", "Biolog\u00EDa", "Historia", "F\u00EDsica", "Ingenier\u00EDa"}));
-				comboBox_1.setBounds(134, 345, 238, 22);
+				comboBox_1.setBounds(192, 345, 297, 22);
 				panelJuez.add(comboBox_1);
 			}
 		}
