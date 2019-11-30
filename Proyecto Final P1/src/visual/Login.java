0f792cff5b601569202cbb6ba060b54dc41205b8
GabLast <50205184+GabLast@users.noreply.github.com>
2019-11-29 21:12:52 -0400

    test

--- Proyecto Final P1/src/visual/Login.java
+++ Proyecto Final P1/src/visual/Login.java
@@ -100,7 +100,7 @@ public class Login extends JFrame {
 	public Login() {
 		setTitle("Iniciando sesi\u00F3n");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 502, 322);
+		setBounds(100, 100, 502, 312);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
